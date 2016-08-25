b9a37ad148dafe08c12b50ecc8506200302832bd
Doug Durham <ddurham@nebraskaglobal.com>
2016-08-25 16:03:48 -0500

    Syntax analysis walkthroughs

--- Syntax Analysis/GettingStartedCS/Program.cs
+++ Syntax Analysis/GettingStartedCS/Program.cs
@@ -14,6 +14,38 @@ namespace GettingStartedCS
     {
         static void Main(string[] args)
         {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+                @"using System;
+                using System.Collections;
+                using System.Linq;
+                using System.Text;
+
+                namespace HelloWorld
+                {
+                    class Program
+                    {
+                        static void Main(string[] args)
+                        {
+                            Console.WriteLine(""Hello, World!"");
+                        }
+                    }
+                }");
+
+            // Manual Traversal
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var firstMember = root.Members[0];
+            var helloWorldDeclaration = (NamespaceDeclarationSyntax)firstMember;
+            var programDeclaration = (ClassDeclarationSyntax)helloWorldDeclaration.Members[0];
+            var mainDeclaration = (MethodDeclarationSyntax)programDeclaration.Members[0];
+            var argsParameter = mainDeclaration.ParameterList.Parameters[0];
+
+            //Query Methods
+            var firstParameters = from methodDeclaration in root.DescendantNodes()
+                                                    .OfType<MethodDeclarationSyntax>()
+                                  where methodDeclaration.Identifier.ValueText == "Main"
+                                  select methodDeclaration.ParameterList.Parameters.First();
+
+            var argsParameter2 = firstParameters.Single();
         }
     }
 }
